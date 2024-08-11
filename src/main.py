52a5afa9392d1fe6a524d970aa88eea15c4a456f
Amir Karimi <amk9978@gmail.com>
2024-08-11 10:11:15 +0330

    fix: Code reformated

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from collections import defaultdict
 from datetime import timedelta
-from typing import List
+from typing import List, Dict, DefaultDict
 
 
 class City:
-
     def __init__(self, id: int, name: str):
         self.id = id
         self.name = name
@@ -17,9 +16,17 @@ class City:
 
 
 class Road:
-
-    def __init__(self, id: int, name: str, from_: int, to: int, through: List[int], speed_limit: int, length: int,
-                 bi_directional: int):
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        from_: int,
+        to: int,
+        through: List[int],
+        speed_limit: int,
+        length: int,
+        bi_directional: int,
+    ):
         self.id = id
         self.name = name
         self.from_ = from_
@@ -43,21 +50,22 @@ class Road:
         return hash(self.id)
 
 
-city_storage = dict()
-road_storage = dict()
-city_roads = defaultdict(list)
+city_storage: Dict[int, City] = {}
+road_storage: Dict[int, Road] = {}
+city_roads: DefaultDict[int, List[Road]] = defaultdict(list)
 
 
 def add_city() -> int:
-    id_inp = int(input("id=?\n"))
+    inp_id = int(input("id=?\n"))
     name_inp = input("name=?\n")
-    city = City(id=id_inp, name=name_inp)
-    city_storage[id_inp] = city
-    return id_inp
+
+    city = City(id=inp_id, name=name_inp)
+    city_storage[inp_id] = city
+    return inp_id
 
 
 def add_road() -> int:
-    id_inp = int(input("id=?\n"))
+    inp_id = int(input("id=?\n"))
     name_inp = input("name=?\n")
     from_inp = int(input("from=?\n"))
     to_inp = int(input("to=?\n"))
@@ -71,75 +79,85 @@ def add_road() -> int:
     length = int(input("length=?\n"))
     bi_directional = int(input("bi_directional=?\n"))
 
-    road = Road(id=id_inp, name=name_inp, from_=from_inp, to=to_inp, through=list(th_cities), speed_limit=speed_limit,
-                length=length, bi_directional=bi_directional)
-
-    road_storage[id_inp] = road
+    road = Road(
+        id=inp_id,
+        name=name_inp,
+        from_=from_inp,
+        to=to_inp,
+        through=list(th_cities),
+        speed_limit=speed_limit,
+        length=length,
+        bi_directional=bi_directional,
+    )
+    road_storage[inp_id] = road
     for city in road.through:
         city_roads[city].append(road)
 
-    return id_inp
+    return inp_id
 
 
-def run_add(command: int):
-    if command == 1:
+def run_add(add_command: int):
+    if add_command == 1:
         id = add_city()
         model = "City"
     else:
         id = add_road()
         model = "Road"
     output_text = "%s with id=%d added!\nSelect your next action\n1. Add another %s\n2. Main Menu\n" % (
-        model, id, model)
+        model,
+        id,
+        model,
+    )
     new_command = int(input(output_text))
     if new_command == 1:
-        run_add(command=command)
+        run_add(add_command=add_command)
 
 
-def delete_city():
-    id_inp = int(input())
-    if id_inp in city_storage:
+def delete_city() -> int:
+    inp_id = int(input())
+    if inp_id in city_storage:
         ss = list(road_storage.keys())
         for road_id in ss:
             road = road_storage[road_id]
             for city in road.through:
-                if city == id_inp:
+                if city == inp_id:
                     del_road(road_id)
                     break
 
-        del city_roads[id_inp]
-        del city_storage[id_inp]
+        del city_roads[inp_id]
+        del city_storage[inp_id]
     else:
-        raise Exception("City with id %d not found!" % id_inp)
-    return id_inp
+        raise ValueError("City with id %d not found!" % inp_id)
+    return inp_id
 
 
-def del_road(id_inp):
-    if id_inp in road_storage.keys():
-        road = road_storage[id_inp]
-        del road_storage[id_inp]
+def del_road(inp_id: int) -> int:
+    if inp_id in road_storage.keys():
+        road = road_storage[inp_id]
+        del road_storage[inp_id]
         for city in road.through:
             city_roads[city].remove(road)
     else:
-        raise Exception("Road with id %d not found!" % id_inp)
+        raise ValueError("Road with id %d not found!" % inp_id)
 
-    return id_inp
-
-
-def delete_road():
-    id_inp = int(input())
-    return del_road(id_inp)
+    return inp_id
 
 
-def run_delete(command: int):
+def delete_road() -> int:
+    inp_id = int(input())
+    return del_road(inp_id=inp_id)
+
+
+def run_delete(del_command: int):
     try:
-        if command == 1:
+        if del_command == 1:
             id = delete_city()
             model = "City"
         else:
             id = delete_road()
             model = "Road"
         print("%s:%d deleted!" % (model, id))
-    except Exception as e:
+    except ValueError as e:
         print(e)
 
 
@@ -148,27 +166,22 @@ def find_roads(origin: int, dest: int) -> List[Road]:
     dest_roads = city_roads[dest]
 
     common_roads = set(origin_roads).intersection(dest_roads)
-    final_roads = set()
-    initial = -1
-    finish = -1
+    valid_roads = []
+
     for road in common_roads:
-        for city in road.through:
-            if city == -1:
-                break
-            if city == origin:
-                initial = origin
-                if finish != -1 and road.bi_directional == 1:
-                    final_roads.add(road)
-                    break
-            elif city == dest:
-                if initial != -1:
-                    final_roads.add(road)
-                    break
+        if origin in road.through and dest in road.through:
+            origin_index = road.through.index(origin)
+            dest_index = road.through.index(dest)
 
-    return list(final_roads)
+            if origin_index < dest_index:
+                valid_roads.append(road)
+            elif road.bi_directional and dest_index < origin_index:
+                valid_roads.append(road)
+
+    return valid_roads
 
 
-def format_time(spent_time):
+def format_time(spent_time: float) -> str:
     deltas = timedelta(hours=spent_time)
     days = deltas.days
     hours, remainder = divmod(deltas.seconds, 3600)
@@ -177,27 +190,29 @@ def format_time(spent_time):
 
 
 def run_path(origin: int, dest: int):
-    roads = find_roads(origin, dest)
-    roads = sorted(roads, reverse=False)
+    roads = find_roads(origin=origin, dest=dest)
+    roads = sorted(roads, reverse=True)
     for road in roads:
-        print("%s:%s via Road %s: Takes %s"
-              % (city_storage[origin].name, city_storage[dest].name, road.name, format_time(road.spent_time)))
+        print(
+            "%s:%s via Road %s: Takes %s"
+            % (city_storage[origin].name, city_storage[dest].name, road.name, format_time(spent_time=road.spent_time))
+        )
 
 
 while True:
-    command_inp = input('Main Menu - Select an action:\n1. Help\n2. Add\n3. Delete\n4. Path\n5. Exit\n')
+    command_inp = input("Main Menu - Select an action:\n1. Help\n2. Add\n3. Delete\n4. Path\n5. Exit\n")
 
     if command_inp == "1":
         print("Select a number from shown menu and enter. For example 1 is for help.")
     elif command_inp == "2":
         command = int(input("Select model:\n1. City\n2. Road\n"))
-        run_add(command)
+        run_add(add_command=command)
     elif command_inp == "3":
         command = int(input("Select model:\n1. City\n2. Road\n"))
-        run_delete(command)
+        run_delete(del_command=command)
     elif command_inp == "4":
         cities = input().split(":")
-        run_path(int(cities[0]), int(cities[1]))
+        run_path(origin=int(cities[0]), dest=int(cities[1]))
     elif command_inp == "5":
         break
     else:
